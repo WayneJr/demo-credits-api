21576e4e01fd896fa9b7162e31258f570224852a
WayneJr <40968414+WayneJr@users.noreply.github.com>
2022-11-09 23:36:50 +0100

    migration updates, new packages, env update

--- migrations/20221104102935_default.ts
+++ migrations/20221104102935_default.ts
@@ -2,17 +2,20 @@ import { Knex } from 'knex';
 import transactionSchema from '../src/core/schemas/transaction.schema';
 import walletSchema from '../src/core/schemas/wallet.schema';
 import userSchema from '../src/core/schemas/user.schema';
+import accountSchema from '../src/core/schemas/account.schema';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema
     .createTable('users', userSchema)
     .createTable('wallets', walletSchema)
+    .createTable('accounts', accountSchema)
     .createTable('transactions', transactionSchema);
 }
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema
-    .dropTableIfExists('users')
+    .dropTableIfExists('transactions')
+    .dropTableIfExists('accounts')
     .dropTableIfExists('wallets')
-    .dropTableIfExists('transactions');
+    .dropTableIfExists('users');
 }
