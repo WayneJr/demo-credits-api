53de5a359e03dc4fb4ff8af7326d5b9e42faf202
WayneJr <40968414+WayneJr@users.noreply.github.com>
2022-11-10 11:38:56 +0100

    test(): users unit tests

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getConnectionToken } from 'nest-knexjs';
 import { UserService } from './user.service';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getConnectionToken(),
+          useClass: class FakeClass {},
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +22,60 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('testing findAll', () => {
+    // it('should return an array of users', async () => {
+    // const result = ['test'];
+    // jest.spyOn(service, 'findAll').mockImplementation(() => result);
+    // expect(await controller.findAll()).toBe([]);
+    // });
+
+    it('should return an array of users', async () => {
+      const spy = jest
+        .spyOn(service, 'findAll')
+        .mockImplementationOnce(async () => ['test']);
+      const response = await service.findAll();
+
+      expect(spy).toHaveBeenCalled();
+      expect(response).toStrictEqual(['test']);
+    });
+
+    it('should throw a internal server error', async () => {
+      jest.spyOn(service, 'findAll').mockImplementationOnce(() => {
+        throw new InternalServerErrorException('Internal Server Error');
+      });
+    });
+  });
+
+  describe('testing create', () => {
+    it('should return a user', async () => {
+      const spy = jest
+        .spyOn(service, 'create')
+        .mockImplementationOnce(async () => {
+          return {
+            message: 'User created successfully',
+          };
+        });
+
+      const response = await service.create({
+        firstName: 'Fred',
+        lastName: 'Johnson',
+        email: 'fred@gmail.com',
+        password: 'dave123',
+        tag: 'freddy',
+        transactionPin: '1234',
+      });
+      expect(spy).toHaveBeenCalled();
+      expect(response).toStrictEqual({
+        message: 'User created successfully',
+      });
+    });
+
+    it('should throw a internal server error', async () => {
+      jest.spyOn(service, 'create').mockImplementationOnce(() => {
+        throw new InternalServerErrorException('Internal Server Error');
+      });
+    });
   });
 });
