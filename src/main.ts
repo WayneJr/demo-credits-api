af10091c74c2f9c939baa4fbb59cc95b24e3c04d
WayneJr <40968414+WayneJr@users.noreply.github.com>
2022-11-10 14:49:11 +0100

    feature(): server heartbeat and configs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { Logger } from '@nestjs/common';
+import { Logger, RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1', {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`Listening on port: ${port}`);
